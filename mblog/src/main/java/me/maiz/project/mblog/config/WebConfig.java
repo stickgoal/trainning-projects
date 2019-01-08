d1559d85a380951a11632bbfb7ae87d3a2db755d
lucas <stick.goal@163.com>
2019-01-08 11:41:50 +0800

    增加分页功能

--- mblog/src/main/java/me/maiz/project/mblog/config/WebConfig.java
+++ mblog/src/main/java/me/maiz/project/mblog/config/WebConfig.java
@@ -1,14 +1,29 @@
 package me.maiz.project.mblog.config;
 
 import me.maiz.project.mblog.component.SecurityInterceptor;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.servlet.ServletContext;
+
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer,InitializingBean {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new SecurityInterceptor()).addPathPatterns("/write/**","/photo/upload");
+    }
+
+    @Autowired
+    private ServletContext servletContext;
+
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("添加属性");
+        servletContext.setAttribute("staticServerAddress","localhost:8888");
     }
 }
--- mblog/src/main/java/me/maiz/project/mblog/web/IndexController.java
+++ mblog/src/main/java/me/maiz/project/mblog/web/IndexController.java
@@ -3,12 +3,16 @@ package me.maiz.project.mblog.web;
 import me.maiz.project.mblog.dal.BlogRepo;
 import me.maiz.project.mblog.domain.Blog;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.validation.constraints.Null;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,24 +23,25 @@ public class IndexController {
     private BlogRepo blogRepo;
 
     @RequestMapping("index")
-    public String index(ModelMap modelmap){
-        modelmap.put("name","lucas");
-        List<Blog> blogs = blogRepo.findAll();
-        modelmap.addAttribute("blogs",blogs);
+    public String index(ModelMap modelmap, @RequestParam(required = false,defaultValue = "0") Integer pageIdx, @RequestParam(required = false, defaultValue = "5") Integer pageSize) {
+        modelmap.put("currIndex", pageIdx);
+        PageRequest pageRequest = PageRequest.of(pageIdx, pageSize);
+        Page<Blog> blogPage = blogRepo.findAll(pageRequest);
+        modelmap.addAttribute("blogPage", blogPage);
         return "index";
     }
 
     @RequestMapping("detail")
-    public String detail(int blogId,ModelMap modelmap){
+    public String detail(int blogId, ModelMap modelmap) {
         Optional<Blog> blog = blogRepo.findById(blogId);
         System.out.println(blog.get());
-        modelmap.addAttribute("blog",blog.get());
+        modelmap.addAttribute("blog", blog.get());
         return "detail";
     }
 
-    @RequestMapping(value="comment",method = RequestMethod.POST)
+    @RequestMapping(value = "comment", method = RequestMethod.POST)
     @ResponseBody
-    public String comment(String comment){
+    public String comment(String comment) {
         return "";
     }
 
